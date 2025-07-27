18598b7ad79b97ffcdf981967b0d8fbd47c37f55
Oleg <o.bruch@icloud.com>
2025-07-27 23:22:16 +0400

    fix

--- app/button.tsx
+++ app/button.tsx
@@ -1,6 +1,31 @@
 "use client";
-import { TonConnectButton } from "@tonconnect/ui-react";
+import { TonConnectButton, useTonConnectUI } from "@tonconnect/ui-react";
+import { useEffect } from "react";
+
+const proof = {
+    token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyYW5kb21fYnl0ZXMiOiI2YjQ1YzZiMjlkMGIxYmQwMGEwMzczMjVjMDg3MTU2N2YxNjJlNjNiNTZlOGM2YTJiMzVkNmJhN2QzMWE1MmJhIiwiZXhwIjoxNzUzNjQ0ODYyfQ.zNawMg0DbkXkyM5DUsdLGKipnDyRR5x9xBhGUAP_X0s",
+    token_hash: "1de63c6f8c204620b95e236997233d85d9d59825421543728306d7ffcd9a0bc9"
+};
 
 export function PayButton() {
+
+    const [connector] = useTonConnectUI();
+    useEffect(() => {
+        connector.setConnectRequestParameters({
+            state: "ready",
+            value: { tonProof: proof.token_hash }
+        });
+    }, [connector]);
+
+    useEffect(() => {
+        const unsubscribe = connector.onStatusChange(walletInfo => {
+            if (walletInfo?.connectItems?.tonProof) {
+                const proof = walletInfo.connectItems.tonProof;
+                console.log('TON Proof:', proof);
+            }
+        });
+
+        return () => unsubscribe();
+    }, [connector]);
     return <TonConnectButton className="scale-110" />;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,8 @@
 import Image from "next/image";
 import { PayButton } from "./button";
 import { Wallet } from "./wallet";
-import { useTonConnectUI } from "@tonconnect/ui-react";
-import { useEffect } from "react";
-
-const proof = {
-  token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyYW5kb21fYnl0ZXMiOiI2YjQ1YzZiMjlkMGIxYmQwMGEwMzczMjVjMDg3MTU2N2YxNjJlNjNiNTZlOGM2YTJiMzVkNmJhN2QzMWE1MmJhIiwiZXhwIjoxNzUzNjQ0ODYyfQ.zNawMg0DbkXkyM5DUsdLGKipnDyRR5x9xBhGUAP_X0s",
-  token_hash: "1de63c6f8c204620b95e236997233d85d9d59825421543728306d7ffcd9a0bc9"
-};
 
 export default function Home() {
-  const [connector] = useTonConnectUI();
-  useEffect(() => {
-    connector.setConnectRequestParameters({
-      state: "ready",
-      value: { tonProof: proof.token_hash }
-    });
-  }, [connector]);
-
-  useEffect(() => {
-    const unsubscribe = connector.onStatusChange(walletInfo => {
-      if (walletInfo?.connectItems?.tonProof) {
-        const proof = walletInfo.connectItems.tonProof;
-        console.log('TON Proof:', proof);
-      }
-    });
-
-    return () => unsubscribe();
-  }, [connector]);
   return (
     <div className="min-h-screen flex flex-col items-center justify-center px-6 py-16 gap-12 bg-gradient-to-b from-white to-gray-100 dark:from-black dark:to-neutral-900">
       <Image
