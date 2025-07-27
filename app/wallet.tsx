a3ef66339bf53502692003abc7101d0d71051440
Oleg <o.bruch@icloud.com>
2025-07-27 13:22:47 +0400

    connector fixed

--- app/wallet.tsx
+++ app/wallet.tsx
@@ -1,45 +1,9 @@
 "use client";
-import { useTonWallet } from '@tonconnect/ui-react';
-import { TonConnect, IStorage } from "@tonconnect/sdk";
-class SmartStorage implements IStorage {
-    private fallback = new Map<string, string>();
-    private hasLocalStorage: boolean;
-
-    constructor() {
-        this.hasLocalStorage = typeof window !== "undefined" && typeof window.localStorage !== "undefined";
-    }
-
-    async setItem(key: string, value: string): Promise<void> {
-        if (this.hasLocalStorage) {
-            localStorage.setItem(key, value);
-        } else {
-            this.fallback.set(key, value);
-        }
-    }
-
-    async getItem(key: string): Promise<string | null> {
-        if (this.hasLocalStorage) {
-            return localStorage.getItem(key);
-        } else {
-            return this.fallback.get(key) ?? null;
-        }
-    }
-
-    async removeItem(key: string): Promise<void> {
-        if (this.hasLocalStorage) {
-            localStorage.removeItem(key);
-        } else {
-            this.fallback.delete(key);
-        }
-    }
-}
-export const connector = new TonConnect({
-  manifestUrl: "https://sibstark.github.io/ton-connect-test/tonconnect-manifest.json",
-  storage: new SmartStorage()
-});
+import { useTonWallet, useTonConnectUI } from '@tonconnect/ui-react';
 
 export const Wallet = () => {
     const wallet = useTonWallet();
+    const [connector] = useTonConnectUI();
     const login = async () => {
         const challenge = "login-abc123";
 
