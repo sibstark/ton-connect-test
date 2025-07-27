a4cb85ef8687148fea93feb0c142df3fee58d039
Oleg <o.bruch@icloud.com>
2025-07-27 13:11:11 +0400

    connector

---
+++ app/connector.ts
@@ -0,0 +1,4 @@
+import { TonConnect } from "@tonconnect/sdk";
+export const connector = new TonConnect({
+  manifestUrl: "https://sibstark.github.io/ton-connect-test/tonconnect-manifest.json",
+});
--- app/wallet.tsx
+++ app/wallet.tsx
@@ -1,15 +1,32 @@
 "use client";
 import { useTonWallet } from '@tonconnect/ui-react';
+import { connector } from "./connector";
 
 export const Wallet = () => {
     const wallet = useTonWallet();
+    const login = async () => {
+        const challenge = "login-abc123";
 
+        const result = await connector.signData({
+            type: "text",
+            text: challenge,
+        });
+
+        console.log("result:", result); // base64
+        alert(result);
+    };
     return (
         wallet && (
-            <div>
-                <span>Connected wallet: {wallet?.account?.address}</span>
-                <span>Device: {wallet?.device?.appName}</span>
+            <div className="flex flex-col gap-4">
+                <div>
+                    <span>Connected wallet: {wallet?.account?.address}</span>
+                </div>
+                <div>
+                    <span>Device: {wallet?.device?.appName}</span>
+                </div>
+                <button onClick={login}>login</button>
             </div>
+
         )
     );
 };
