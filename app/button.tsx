4f3c88cb3267d1a8ebb12c29503113f77245098b
Oleg <o.bruch@icloud.com>
2025-07-28 20:46:50 +0400

    new token

--- app/button.tsx
+++ app/button.tsx
@@ -3,8 +3,8 @@ import { TonConnectButton, useTonConnectUI } from "@tonconnect/ui-react";
 import { useEffect } from "react";
 
 const proof = {
-    token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyYW5kb21fYnl0ZXMiOiI2YjQ1YzZiMjlkMGIxYmQwMGEwMzczMjVjMDg3MTU2N2YxNjJlNjNiNTZlOGM2YTJiMzVkNmJhN2QzMWE1MmJhIiwiZXhwIjoxNzUzNjQ0ODYyfQ.zNawMg0DbkXkyM5DUsdLGKipnDyRR5x9xBhGUAP_X0s",
-    token_hash: "1de63c6f8c204620b95e236997233d85d9d59825421543728306d7ffcd9a0bc9"
+    token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyYW5kb21fYnl0ZXMiOiJkOTQ2Yzc3NWNhYzI1Nzk2NGJiOTljMDQzZTczNGNhMzI4N2QwM2M1M2M3MDQ4ZmJmNWI1NmFkOTc4YjFhNDFiIiwiZXhwIjoxNzUzNzIyMDQxfQ.3RuwnHGJlUd3lGbF3FlrMnJOtjF_IKaa8YRH4hnjzCo",
+    token_hash: "29e94132ae61b5686e3b8b3584ac4cd1d85825ecdec66f1eb2c0b39e4093bd18"
 };
 
 export function PayButton() {
