7fd5c171823de4c9d169ecc03bc0199fe36c6f42
Oleg <o.bruch@icloud.com>
2025-07-29 23:16:29 +0400

    new hash

--- app/button.tsx
+++ app/button.tsx
@@ -3,8 +3,7 @@ import { TonConnectButton, useTonConnectUI } from "@tonconnect/ui-react";
 import { useEffect } from "react";
 
 const proof = {
-    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyYW5kb21fYnl0ZXMiOiI5YzQyNGRlYjg0MGI3YjdmM2UwZWYzZTc3YWYyNWExMTM1MWQ2MTBhNmExMTUyMGZlZTJkY2RjYzEyZTY3MTM1IiwiZXhwIjoxNzUzNzc2NTg4fQ.mdTnUtQqQF3zOmMkMUKxdIivv1q14biVifrYugnsChk",
-    "token_hash": "78c8183ff8b2ada64672c3f7b340aae072514eb9f5782c76308b15d44759e5a2"
+    "token_hash": "b455903996327db70000000068892bf527fea613f25274500a5292f1f4e24c83"
 };
 
 export function PayButton() {
